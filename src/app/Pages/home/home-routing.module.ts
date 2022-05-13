7536ad63be6a53780962aa0afc47072ea46819b6
UsielOM <usiel31197@outlook.es>
2022-05-13 00:06:29 -0500

    Colocando el componente conocenos

--- src/app/Pages/home/home-routing.module.ts
+++ src/app/Pages/home/home-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConocenosComponent } from './components/conocenos/conocenos.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { HomepageComponent } from './homepage.component';
 
 const routes: Routes = [
   {path: '', component:HomepageComponent,
 children:[
+  
   {path:'', component:PrincipalComponent}
+  
+  
 ]}];
 
 @NgModule({
