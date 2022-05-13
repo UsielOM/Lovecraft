279119bbc5797b4db0e02030dfe51be218e3c452
UsielOM <usiel31197@outlook.es>
2022-05-12 21:03:24 -0500

    Cree el componente principal para la HomePage

--- src/app/Pages/home/home.module.ts
+++ src/app/Pages/home/home.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomepageComponent } from './homepage.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
 
 @NgModule({
   declarations: [
     HomepageComponent,
-    NavbarComponent
+    NavbarComponent,
+    PrincipalComponent
   ],
   imports: [
     CommonModule,
