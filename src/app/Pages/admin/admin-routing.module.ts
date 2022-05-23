b4b64a3bdfde5b48796f83f99e7bcd946ef5395f
UsielOM <usiel31197@outlook.es>
2022-05-23 17:03:08 -0500

    Creando el componente citas

--- src/app/Pages/admin/admin-routing.module.ts
+++ src/app/Pages/admin/admin-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminpageComponent } from './adminpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:AdminpageComponent,
+children:[
+  { }
+]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
