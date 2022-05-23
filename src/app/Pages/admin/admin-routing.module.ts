d47370c70b2e034dd945fda05817e785ad417e78
UsielOM <usiel31197@outlook.es>
2022-05-23 17:03:46 -0500

    Ruta Hija citas

--- src/app/Pages/admin/admin-routing.module.ts
+++ src/app/Pages/admin/admin-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminpageComponent } from './adminpage.component';
+import { CitasComponent } from './components/citas/citas.component';
 
 const routes: Routes = [
   {path:'', component:AdminpageComponent,
 children:[
-  { }
+  {path:'citas',component:CitasComponent }
 ]
 }
 ];
