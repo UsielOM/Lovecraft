9b4b4a538d062cb7bd7085851e9d6994450e25d5
UsielOM <usiel31197@outlook.es>
2022-05-23 17:03:18 -0500

    Generando el componente citas

--- src/app/Pages/admin/admin.module.ts
+++ src/app/Pages/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminpageComponent } from './adminpage.component';
+import { CitasComponent } from './components/citas/citas.component';
 
 
 @NgModule({
   declarations: [
-    AdminpageComponent
+    AdminpageComponent,
+    CitasComponent
   ],
   imports: [
     CommonModule,
