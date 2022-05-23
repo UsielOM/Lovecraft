8d48853b057802f36d156d5698c24c3f8ce25bd7
UsielOM <usiel31197@outlook.es>
2022-05-23 17:10:54 -0500

    navbar admin componente

--- src/app/Pages/admin/admin.module.ts
+++ src/app/Pages/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminpageComponent } from './adminpage.component';
 import { CitasComponent } from './components/citas/citas.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AdminpageComponent,
-    CitasComponent
+    CitasComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
