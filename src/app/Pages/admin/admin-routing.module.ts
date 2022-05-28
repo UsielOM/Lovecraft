fd7a6e1bd4e6c888fb1aa8db582d49026ff8b460
bjuliatrejo <101273886+bjuliatrejo@users.noreply.github.com>
2022-05-27 19:51:13 -0600

    Agregue el path "perfil" en el routing de admin

--- src/app/Pages/admin/admin-routing.module.ts
+++ src/app/Pages/admin/admin-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminpageComponent } from './adminpage.component';
 import { CitasComponent } from './components/citas/citas.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
 const routes: Routes = [
   {path:'', component:AdminpageComponent,
 children:[
-  {path:'citas',component:CitasComponent }
+  {path:'citas',component:CitasComponent },
+  {path:'perfil', component:PerfilComponent }
 ]
 }
 ];
